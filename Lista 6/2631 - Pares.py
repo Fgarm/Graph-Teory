5adba5bf8104aa868ceba1e3d0e416ba57d4d071
Fgarm <guilhermear.maturana@outlook.com>
2023-05-09 15:25:04 -0300

    Made it faster, without union find, not enough

--- Lista 6/2631 - Pares.py
+++ Lista 6/2631 - Pares.py
@@ -7,36 +7,34 @@ class Graph():
         
 
     def conectar(self, node1, node2):
-        node1 = node1 - 1
-        node2 = node2 - 1
-        self.vertices[node1][node2] = 1
-        self.vertices[node2][node1] = 1
+        self.vertices[node1 -1][node2] = 1
+        self.vertices[node2 -1][node1] = 1
     
     def componentes(self, aluno1, aluno2):
-        ja_passados = set()
-
-        for vertice in range(len(self.vertices)):
-            fila_de_busca = []
+            ja_passados = set()
             componente = set()
-            if vertice not in ja_passados:
-                componente.add(vertice + 1)
-                fila_de_busca.append(vertice)
-                ja_passados.add(vertice)
+            #print("Procurando no componente do:", aluno1)
+            fila_de_busca = [aluno1]
+            #componente.add(aluno1)
+            ja_passados.add(aluno1)
             while fila_de_busca:
                 fila_de_busca_temp = []
                 for entrada in fila_de_busca:
-                    for component in self.vertices[entrada]:
+                    #print("Para o número:", entrada)
+                    if aluno2 in self.vertices[entrada -1]:
+                        print("S")
+                        return
+                    for component in self.vertices[entrada -1]:
+                        #print("Ele conecta com:", component)
                         if component not in ja_passados:
-                            fila_de_busca_temp.append(component)
-                            componente.add(component + 1)
+                            #print("E é a primeira vez passando nele")
                             ja_passados.add(component)
+                            componente.add(component)
+                            fila_de_busca_temp.append(component)
                 fila_de_busca = fila_de_busca_temp
-            if componente:
-                #print(componente)
-                #print((aluno1 in componente) != (aluno2 in componente))
-                if (aluno1 in componente) != (aluno2 in componente):
-                    return False
-        return True
+            #print(componente)
+            print("N")
+
 
 ###################
 while True:
@@ -44,14 +42,9 @@ while True:
         num_estudantes, n_amizades, n_perguntas = [int(a) for a in input().split()]
         sala = Graph(num_estudantes)
         for i in range(n_amizades):
-            aluno1, aluno2 = [int(aluno) for aluno in input().split()] 
-            sala.conectar(aluno1, aluno2)
+            sala.conectar(*[int(aluno) for aluno in input().split()])
 
         for i in range(n_perguntas):
-            aluno1, aluno2 = [int(aluno) for aluno in input().split()] 
-            if sala.componentes(aluno1, aluno2):
-                print("S")
-            else:
-                print("N")
+            sala.componentes(*[int(aluno) for aluno in input().split()])
     except EOFError:
         break
