07d7a65aea9ffbb1c3e109424000d51f63cce943
Fgarm <guilhermear.maturana@outlook.com>
2023-05-25 19:31:54 -0300

    Finish: Ex 1774

--- Lista 6/1774 - Roteadores.py
+++ Lista 6/1774 - Roteadores.py
@@ -1,22 +1,17 @@
-# pegar o grafo
-# gerar a arvore geradora minima
-# usar algoritmo de primm, e no final somar o vetor de custo
-# printar o resultado
-
 class Graph():
     def __init__(self, n_vertices: int) -> None:
         self.N_VERTICES: int = n_vertices
-        self.vertices = []
+        self.vertices: list[dict[int, int]] = []
         for i in range(n_vertices):
             self.vertices.append({})
 
-    def addAresta(self, vertice1, vertice2):
-        self.vertices[vertice1 -1][vertice2 -1] = 1
-        self.vertices[vertice2 -1][vertice1 -1] = 1
+    def addAresta(self, vertice1: int, vertice2: int, custo: int):
+        self.vertices[vertice1 -1][vertice2 -1] = custo
+        self.vertices[vertice2 -1][vertice1 -1] = custo
 
     def arvoreMinima(self):
         # algoritmo de primm:
-        a_passar = {}
+        a_passar: dict[int, int] = {}
         # guarda os vertices que ainda não foram passados como chave, 
         # e o custo até eles como valor.
 
@@ -47,24 +42,11 @@ class Graph():
                         # atualizar o custo dele
                         a_passar[vizinho] = self.vertices[proximo][vizinho] 
             del a_passar[proximo]
-        
         return custo
 
 
-        
-        
-        # gerar um set de vertices a passar
-        # gerar um vetor de custos > 10000
-        
-        # comecar do primeiro vertice e mudar seu custo pra 0
-        
-        # mudar os custos pros vizinhos
-        # tirar esse vertice do set
-        
-        # ver o vertice de menor custo ainda não passado
-
-        # mudar o custo pros vizinhos se forem menores
-        # repetir até não ter mais vertices a passar
-        
-        # melhoria: vetor de custos sendo dict, e remover a entrada já passada do vetor de custos?
-        pass
+routers, cables = [int(entrada) for entrada in input().split()]
+rede = Graph(routers)
+for aresta in range(cables):
+    rede.addAresta(*[int(entrada) for entrada in input().split()])
+print(rede.arvoreMinima())
